c72d3a9b7127342a5f64bfc20640066748c047a0
stephenricks <kennethpatrick.trivino@summitmedia.com.ph>
2018-02-17 12:27:33 +0800

    changed url

--- app/views/app/default/script/script_bottom.blade.php
+++ app/views/app/default/script/script_bottom.blade.php
@@ -1,6 +1,6 @@
 @section('script-bottom')
-<script type="text/javascript" src="{{ url('plugins/swipe/js/jquery.min.js') }}"></script>
-<script type="text/javascript" src="{{ url('plugins/swipe/js/jquery.transform2d.js') }}"></script>
-<script type="text/javascript" src="{{ url('plugins/swipe/js/jquery.jTinder.js') }}"></script>
-<script type="text/javascript" src="{{ url('plugins/swipe/js/main.js') }}"></script>
+<script type="text/javascript" src="/plugins/swipe/js/jquery.min.js"></script>
+<script type="text/javascript" src="/plugins/swipe/js/jquery.transform2d.js"></script>
+<script type="text/javascript" src="/plugins/swipe/js/jquery.jTinder.js"></script>
+<script type="text/javascript" src="/plugins/swipe/js/main.js"></script>
 @show
--- app/views/app/default/script/script_top.blade.php
+++ app/views/app/default/script/script_top.blade.php
@@ -1,4 +1,4 @@
 @section('script_top')
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="{{ url('plugins/swipe/css/jTinder.css') }}">
+<link rel="stylesheet" type="text/css" href="/plugins/swipe/css/jTinder.css">
 @show
